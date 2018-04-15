188cdc4dee61c0d69dd4ace276987f55310b7e35
Arrey <lin2k@live.com>
2018-04-15 15:47:14 +0800

    second commit

--- start.js
+++ start.js
@@ -2,7 +2,7 @@
 "use strict";
 var headlessWallet = require('trustnote-headless');
 var eventBus = require('trustnote-common/event_bus.js');
-var assetUtils = require('./assetManager.js');
+var assetUtils = require('./assetUtils.js');
 var fs = require('fs');
 headlessWallet.setupChatEventHandlers();
 eventBus.on('headless_wallet_ready', function(){
